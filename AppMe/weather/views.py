d540ffa7fd2b69639ae4ce63bf5a592ef05d8090
kirill_kiselev99 <kisabot99@gmail.com>
2023-07-19 01:34:32 +0300

    -add view and weather files to the weather app

--- AppMe/weather/views.py
+++ AppMe/weather/views.py
@@ -1,3 +1,14 @@
+import datetime
+
+import requests
 from django.shortcuts import render
 
+def index(request):
+    API_KEY = open("hidden/API_KEY.txt", "r")
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+
+    if request.method == 'POST':
+
+    else:
+        return render('')
 # Create your views here.
