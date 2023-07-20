4e99e6f1be5b49bff11598fbb594c09cefc402eb
kirill_kiselev99 <kisabot99@gmail.com>
2023-07-20 05:09:31 +0300

    -modify view, forms using weather API

--- AppMe/weather/admin.py
+++ AppMe/weather/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserCity
+
+admin.site.register(UserCity)
 
 # Register your models here.
--- AppMe/weather/urls.py
+++ AppMe/weather/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import city, find_weather
+from .views import get_city #find_weather
 
 
 
 APP_NAME = 'weather'
 urlpatterns = [
-    path('weather/', city, name='city'),
+    path('city', get_city, name='city'),
     #path('weather/users',find_weather, name='users-weather'),
 ]
