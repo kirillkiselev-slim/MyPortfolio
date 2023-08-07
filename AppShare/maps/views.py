1fede93a9f45b22caa01c378293ca4c4c8f2fc4d
kirill_kiselev99 <kisabot99@gmail.com>
2023-08-07 14:42:11 +0300

    -delete unnecessary lines of code inside the maps/views.py file

--- AppShare/maps/views.py
+++ AppShare/maps/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
 from .forms import UserCountry
 from django.contrib import messages
 from .models import Location, UserLocation
@@ -29,21 +28,14 @@ def country(request):
 @login_required()
 def display_map(request):
     user_countries = UserLocation.objects.all()
-    print(user_countries)
 
-    # Get all countries with their coordinates
     countries = Location.objects.all()
-    print(countries)
 
-    # Create a list to store the usernames and countries
     country_usernames = {}
 
     for user_country in user_countries:
         # Find the corresponding country object
         country = countries.get(country=user_country.user_country)
-
-        # Get the coordinates of the country
-        #coordinates = (country.latitude, country.longitude)
 
         # Add the username to the country's list of usernames
         if country.country not in country_usernames:
