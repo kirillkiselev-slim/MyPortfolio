e93d0f226cd30d7703089f0387d69aeebfe2439e
kirill_kiselev99 <kisabot99@gmail.com>
2023-08-07 14:36:04 +0300

    -delete unnecessary lines of code inside the maps/models.py file

--- AppShare/maps/models.py
+++ AppShare/maps/models.py
@@ -7,18 +7,13 @@ class Location(models.Model):
     latitude = models.FloatField(default=55.7512)
     longitude = models.FloatField(default=37.6174)
 
-
     def __str__(self):
         return self.country
 
 
 class UserLocation(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # country = models.ForeignKey(Location, on_delete=models.CASCADE)
-#
     user_country = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
     def __str__(self):
         return self.user_country
