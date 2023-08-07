9c1ab28daee84389674316803089487abec61ab3
kirill_kiselev99 <kisabot99@gmail.com>
2023-08-07 18:29:38 +0300

    -hide secret key

--- AppShare/AppShare/settings.py
+++ AppShare/AppShare/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)e7k2-q#x2vwp&3cw)e4_6%qirvk8g@&g-+34yz&^k%z9!&8l='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
