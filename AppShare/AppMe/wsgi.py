c590be9845a809b47dbfb114b7d24dfb7d0ebecc
kirill_kiselev99 <kisabot99@gmail.com>
2023-08-07 14:04:12 +0300

    name of app changed everywhere

--- AppShare/AppMe/wsgi.py
+++ AppShare/AppMe/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AppMe project.
+WSGI config for AppShare project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppMe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppShare.settings')
 
 application = get_wsgi_application()
